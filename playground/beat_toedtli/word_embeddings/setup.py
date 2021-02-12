0ee22c37a0bef2100144ebd77acf2897b875ad0e
Beat Tödtli <beat.toedtli@gmail.com>
2021-02-12 15:24:47 +0100

    adding playground and word embeddings notebooks in the cookiecutter template

---
+++ playground/beat_toedtli/word_embeddings/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='word_embeddings',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploring different implementations of word embeddings, and benchmarking them with @mirco\'s tools.',
+    author='Beat Tödtli',
+    license='MIT',
+)
