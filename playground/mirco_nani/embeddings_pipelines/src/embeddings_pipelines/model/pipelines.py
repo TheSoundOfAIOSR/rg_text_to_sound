0d879851cac43794c90e29e0f59f97abac0e9bfd
mirco_nani <mirco.nani@injenia.it>
2021-03-03 22:25:01 +0100

    added skeleton of embeddings_pipelines

---
+++ playground/mirco_nani/embeddings_pipelines/src/embeddings_pipelines/model/pipelines.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+from typing import Any, List, Dict
+import numpy as np
+
+class EmbeddingPipeline(ABC):
+    @abstractmethod
+    def __init__(self, *args: List[PredictionModel], **kwargs: Dict[str, PredictionModel]):
+        """Collects the input models to be used for the embedding task
+
+        Args:
+            *args (List[PredictionModel]): the prediction models needed by the pipeline
+            **kwargs (Dict[str, PredictionModel]): the prediction models needed by the pipeline
+
+        """
+        pass
+
+  @abstractmethod
+  def build(self):
+    """ This method builds the pipeline that will embed the sentences
+    """
+    pass
+
+    @abstractmethod
+    def embed(self, sentence:str) -> np.array:
+        """Embeds a sentence in a low-dimensional array
+
+        Args:
+            sentence (str): a sentence
+
+        Returns:
+            np.array: a 1-D numpy array of floats
+        """
