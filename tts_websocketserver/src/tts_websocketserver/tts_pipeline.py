09b0a0a55d220077e5b4fd3956508e75129b60dc
nani.mirco <nani.mirco@gmail.com>
2021-05-30 14:15:29 +0200

    fixed import

--- tts_websocketserver/src/tts_websocketserver/tts_pipeline.py
+++ tts_websocketserver/src/tts_websocketserver/tts_pipeline.py
@@ -1,5 +1,5 @@
 from tts_pipeline.pipelines.waterfall.pipeline import WaterfallPipeline
-from tts_pipeline.pipelines.waterfall.models.UnifiedKeywordExtractor import UnifiedKeywordExtractor
+from tts_pipeline.pipelines.waterfall.models.UnifiedKeywordExtractor import UnifiedKeywordPairsExtractor
 from tts_pipeline.pipelines.waterfall.models.gnews_models import GNewsWaterfallEmbedder
 from tts_pipeline.pipelines.waterfall.models.examples import DummyWaterfallDimensionalityReducer
 from tts_websocketserver.utils import assets_folder
